6c811d9fceed9d937d38ea8af038eafa77fc2f65
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-08-12 10:06:09 +0100

    Add created_at field to product model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Integer, Column, String, Float
+from sqlalchemy import Integer, Column, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
@@ -10,11 +11,10 @@ class Product(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     brand = Column(String, index=True)
+    color = Column(String, index=True)
+    storage = Column(Integer, index=True)
     description = Column(String)
     image_url = Column(String)
-    color = Column(String)
-    storage = Column(String)
     price = Column(Float)
     quantity = Column(Integer)
-
-
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
